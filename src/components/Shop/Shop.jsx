159da5fac364e16b99bbcd0c7f02a3d16451e827
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-03-29 04:30:35 +0600

    product card design

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
@@ -18,7 +19,14 @@ const Shop = () => {
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h2>Products Coming Here : {products.length}</h2>
+                {
+                    products.map(product => <Product
+                    key ={product.id}
+                    product = {product}>
+
+                    </Product>)
+                }
+
             </div>
 
             <div className="cart-container">
